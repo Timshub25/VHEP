80e1cb9119ec5fc1f50f82f955dddbd142db00f6
Timshub25 <timwall18@gmail.com>
2020-05-29 13:56:42 +1000

    added publications content

--- app.js
+++ app.js
@@ -23,9 +23,24 @@ burger.addEventListener("click", () => {
   line2.classList.toggle("active");
   burger.classList.toggle("fixed");
 });
+
+//experimenting with dropdown hover
+
+// const sub = get(".sub");
+// const submenus = getAll(".submenu");
+// sub.addEventListener("mouseover", (e) => {
+//   submenus[0].style.display = "block";
+//   e.preventDefault();
+// });
+// sub.addEventListener("mouseleave", (e) => {
+//   submenus[0].style.display = "none";
+// });
+
+//experimnting with clicking on nav
+
 // //Drop down nav  for about
 // const about = get(".about");
-// const submenu1 = get(".submenu1");
+// const submenu1 = get(".submenu");
 // const triangle1 = get(".triangle1");
 // about.addEventListener("click", (e) => {
 //   e.preventDefault();
@@ -35,10 +50,12 @@ burger.addEventListener("click", () => {
 
 // //Drop down nav  for research
 // const research = get(".research");
-// const submenu2 = get(".submenu2");
+// const submenu2 = get(".submenu");
 // const triangle2 = get(".triangle2");
 // research.addEventListener("click", (e) => {
 //   e.preventDefault();
 //   submenu2.classList.toggle("active");
 //   triangle2.classList.toggle("flipped");
 // });
+
+//Use data is click?
