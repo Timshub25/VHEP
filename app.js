9234a5e89f4a9b642f9114dcbae25237d69cae23
Tim Wall <timwall18@gmail.com>
2020-07-02 09:53:39 +1000

    added another team member

--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 const get = (element) => document.querySelector(element);
 const getAll = (element) => document.querySelectorAll(element);
 
-//resize square
-window.addEventListener("resize", (e) => {
-  let screenWidth = get(".screen-width");
-  screenWidth.innerText = window.innerWidth;
-});
-
 //GSAP
 const tl = gsap.timeline();
 
@@ -23,39 +17,3 @@ burger.addEventListener("click", () => {
   line2.classList.toggle("active");
   burger.classList.toggle("fixed");
 });
-
-//experimenting with dropdown hover
-
-// const sub = get(".sub");
-// const submenus = getAll(".submenu");
-// sub.addEventListener("mouseover", (e) => {
-//   submenus[0].style.display = "block";
-//   e.preventDefault();
-// });
-// sub.addEventListener("mouseleave", (e) => {
-//   submenus[0].style.display = "none";
-// });
-
-//experimnting with clicking on nav
-
-// //Drop down nav  for about
-// const about = get(".about");
-// const submenu1 = get(".submenu");
-// const triangle1 = get(".triangle1");
-// about.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   submenu1.classList.toggle("active");
-//   triangle1.classList.toggle("flipped");
-// });
-
-// //Drop down nav  for research
-// const research = get(".research");
-// const submenu2 = get(".submenu");
-// const triangle2 = get(".triangle2");
-// research.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   submenu2.classList.toggle("active");
-//   triangle2.classList.toggle("flipped");
-// });
-
-//Use data is click?
